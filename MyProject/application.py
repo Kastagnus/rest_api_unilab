f524ef8a477f63087ace142e9ea030e33f01560f
Kastagnus <ucha95@gmail.com>
2021-02-22 19:49:05 +0400

    new commit

--- MyProject/application.py
+++ MyProject/application.py
@@ -1,16 +1,16 @@
 from flask import Flask, redirect
 from flask_restful import Api
 from flask_jwt import JWT
-from new_security import authentication, identity
+# from security import authentication, identity
 from MyProject.resources.hotel_rooms import All_Room, Room
-from MyProject.resources.user import RegisterUser
+# from MyProject.resources.user import RegisterUser
 
 app = Flask(__name__)
 app.secret_key = "New_Secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///hotelrooms.db"
 app.config['PROPAGATE_EXCEPTIONS'] = True
 appI = Api(app)
-jwt = JWT(app, authentication, identity)
+# jwt = JWT(app, authentication, identity)
 
 @app.before_first_request
 def create_table():
@@ -20,13 +20,13 @@ def create_table():
 @app.route("/")
 def home():
 
-    return redirect("github link"), 302
+    return redirect("https://github.com/Kastagnus/rest_api_unilab/tree/main"), 302
 
 appI.add_resource(All_Room, "/about/")
 appI.add_resource(Room, "/about/<string:room_type>")
-appI.add_resource(RegisterUser, "/registration/")
+# appI.add_resource(RegisterUser, "/registration/")
 
 if __name__ == "__main__":
-    from db import db
+    from MyProject.db import db
     db.init_app(app)
     app.run(debug=True, port=5100)
