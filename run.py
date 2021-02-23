3390fb4fb11c462746262a08e6b6f061d88c21ec
Kastagnus <ucha95@gmail.com>
2021-02-24 00:10:43 +0400

    okok

--- run.py
+++ run.py
@@ -1,13 +1,9 @@
-# from MyProject.application import app
-# from MyProject.db import db
-#
-# db.init_app(app)
-#
-# @app.before_first_request
-# def create_table():
-#     db.create_all()
-#
-# if __name__ == "__main__":
-#
-#     app.run(debug=True, port=5100)
+from MyProject.application import app
+from MyProject.db import db
+
+db.init_app(app)
+
+@app.before_first_request
+def create_table():
+    db.create_all()
 
