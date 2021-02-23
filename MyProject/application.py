3a878f0e299dfbe64d2b01c1333aa39b12cb5fba
Kastagnus <ucha95@gmail.com>
2021-02-24 00:14:37 +0400

    okok

--- MyProject/application.py
+++ MyProject/application.py
@@ -12,10 +12,6 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 appI = Api(app)
 jwt = JWT(app, authentication, identity)
 
-@app.before_first_request
-def create_table():
-    db.create_all()
-
 @app.route("/")
 def home():
 
